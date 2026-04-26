312b8896a1766cebcdd020337a486495e5ca8860
bouchrabenkhelifa <benbouchra317@gmail.com>
2026-04-26 22:23:51 +0100

    add RL pipeline for GRU TIMESNET DLINEAR

--- src/models/RL/__init__.py
+++ src/models/RL/__init__.py
@@ -1,4 +1,4 @@
-from src.models.RL.actor  import Actor
-from src.models.RL.critic import Critic
-from src.models.RL.reward import CFReward
-from src.models.RL.agent  import ActorCritic, build_state
+from src.models.RL.actor      import Actor
+from src.models.RL.critic     import Critic
+from src.models.RL.reward_last import CFReward
+from src.models.RL.agent      import ActorCritic, build_state
